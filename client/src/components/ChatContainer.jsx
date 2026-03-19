a3f73878d1a271a5b7096050bb071e1bee9c8805
Karunasindhu Adak <karunasindhuadak7@gmail.com>
2026-03-19 18:32:08 +0530

    Implement chat functionality with message formatting and user sidebar

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-import assets from '../assets/assets'
+import React, { useEffect, useRef } from 'react'
+import assets, { messagesDummyData } from '../assets/assets'
+import { formatMessageTime } from '../lib/util'
 
 const ChatContainer = ({selectedUser, setSelectedUser}) => {
+  const scrollEnd = useRef()
+  useEffect(() => {
+    if(scrollEnd.current) {
+      scrollEnd.current.scrollIntoView({ behavior: "smooth" })
+    }
+  }, [])
+
   return selectedUser ? (
-    <div>
+    <div className='h-full relative overflow-scroll backdrop-blur-lg'>
       {/* Chat Header */}
       <div className='flex items-center gap-3 py-3 mx-4 border-b border-stone-300'>
         <img src={assets.profile_martin} alt="" className='w-8 h-8 rounded-full'/>
@@ -13,6 +21,39 @@ const ChatContainer = ({selectedUser, setSelectedUser}) => {
         </p>
         <img onClick={() => (setSelectedUser(null))} src={assets.arrow_icon} alt="" className='md:hidden max-w-7'/>
         <img src={assets.help_icon} alt="" className='max-md:hidden max-w-5'/>
+      </div>
+      {/* Chat Body */}
+      <div className='flex flex-col h-[calc(100%-120px)] p-3 pb-6 overflow-y-scroll'>
+          {messagesDummyData.map((msg, index) => (
+            <div key={index} className={`flex items-end justify-end gap-2 ${msg.senderId !== '680f50e4f10f3cd28382ecf9' && 'flex-row-reverse'}`}>
+              {msg.image ? (
+              <img src={msg.image} alt="" className='max-w-[230px] border border-gray-700 rounded-lg overflow-hidden mb-8'/>
+              ):(
+              <p className={`mb-8 p-2 max-w-[200px] md:text-sm rounded-lg break-all bg-violet-500/30 font-light text-white ${msg.senderId === '680f50e4f10f3cd28382ecf9' ? 'rounded-br-none' : 'rounded-bl-none'}`}>{msg.text}</p>
+              )}
+
+              <div className='text-center text-xs'>
+                <img src={msg.senderId === '680f50e4f10f3cd28382ecf9' ? assets.avatar_icon : assets.profile_martin} alt="" className='w-7 rounded-full'/>
+                <p className='text-gray-500'>{formatMessageTime(msg.createdAt)}</p>
+              </div>
+            </div>
+          ))}
+          <div ref={scrollEnd}></div>
+      </div>
+
+      {/* Bottom area */}
+
+      <div className='absolute bottom-0 left-0 right-0 p-3 gap-3 flex items-center mt-2'>
+        <div className='flex-1 flex items-center rounded-full bg-gray-100/12 px-3'>
+          <input type="text" placeholder='Send a message...'
+          className='flex-1 border-none outline-none p-3 rounded-lg placeholder-gray-400 text-white'
+          />
+          <input type="file" name="" id="image" accept='image/png, image/jpeg' hidden/>
+          <label htmlFor="image">
+            <img src={assets.gallery_icon} alt="" className='w-5 mr-2 cursor-pointer'/>
+          </label>
+        </div>
+        <img src={assets.send_button} alt="" className='w-7 cursor-pointer'/>
       </div>
     </div>
   ) : (
